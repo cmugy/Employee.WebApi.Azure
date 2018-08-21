75d7fd580125afd82584d47ae323c2a311c2bc95
Collins Mugarura <collismugy@hotmail.com>
2018-08-21 22:48:54 +0100

    Added more functionality

--- Employee.WebApi.Azure/Controllers/EmployeeController.cs
+++ Employee.WebApi.Azure/Controllers/EmployeeController.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Employee.WebApi.Azure.DataAccess;
+using Employee = Employee.WebApi.Azure.Models.Employee;
 
 namespace Employee.WebApi.Azure.Controllers
 {
-    public class EmployeeController : ApiController
+    public class EmployeeController : ControllerBase
     {
+        private IEmployeeDataService _employeeDataService = new EmployeeDataService(); // todo use container
 
+        public EmployeeController(IEmployeeDataService employeeDataService)
+        {
+            _employeeDataService = employeeDataService;
+        }
+         
+        [HttpGet, Route("api/employees")]
+        public IEnumerable<Models.Employee> GetAllEmployees()
+        {
+            return new List<Models.Employee>
+            {
+                new Models.Employee
+                {
+                    Id = 1,
+                    FirstName = "Collins",
+                    LastName = "Mugarura",
+                    Salary = 43000,
+                    StartDate = new DateTime(2017, 11,09)
+                }
+            };
+        }
     }
 }
