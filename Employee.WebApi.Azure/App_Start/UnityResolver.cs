da481444695c3f92110d57d89e034f089235705b
Collins Mugarura <collismugy@hotmail.com>
2018-08-22 20:21:38 +0100

    Added unity container

---
+++ Employee.WebApi.Azure/App_Start/UnityResolver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Unity;
+using Unity.Exceptions;
+
+namespace Employee.WebApi.Azure.App_Start
+{
+    public class UnityResolver : IDependencyResolver
+    {
+        protected IUnityContainer container;
+
+
+        //this is how we can use Ninject
+        //https://nodogmablog.bryanhogan.net/2016/04/web-api-2-and-ninject-how-to-make-them-work-together/
+
+        public UnityResolver(IUnityContainer container)
+        {
+            this.container = container ?? throw new ArgumentNullException(nameof(container));
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                var service = container.Resolve(serviceType);
+
+                return service;
+            }
+            catch (ResolutionFailedException e)
+            {
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException e)
+            {
+                return new List<object>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            container.Dispose();
+        }
+    }
+}
--- Employee.WebApi.Azure/App_Start/WebApiConfig.cs
+++ Employee.WebApi.Azure/App_Start/WebApiConfig.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Employee.WebApi.Azure.App_Start;
+using Employee.WebApi.Azure.DataAccess;
+using Employee.WebApi.Azure.Services;
+using Unity;
 
 namespace Employee.WebApi.Azure
 {
@@ -10,6 +14,13 @@ namespace Employee.WebApi.Azure
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            var container = new UnityContainer();
+
+            container.RegisterType<ILogProvider, LogProvider>();
+            container.RegisterType<IEmployeeDataService, EmployeeDataService>();
+
+            config.DependencyResolver= new UnityResolver(container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
